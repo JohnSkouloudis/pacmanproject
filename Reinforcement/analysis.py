66b2f5afe181e0f17dd025f4757b0bb35feef5a3
John skoul <johnskoul56@gmail.com>
2026-04-10 00:11:01 +0300

    q3a done

--- Reinforcement/analysis.py
+++ Reinforcement/analysis.py
@@ -25,9 +25,9 @@ def question2():
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.3
+    answerNoise = 0.0
+    answerLivingReward = 0.0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
